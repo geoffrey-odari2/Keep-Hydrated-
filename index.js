5493b938c35bef6a3de5b3cf5086295548fa199e
geoffrey-odari2 <92371059+geoffrey-odari2@users.noreply.github.com>
2021-11-09 13:34:04 +0000

    changed the name of the parameter to time

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 //function to calculate amount of water to drink based on the time taken based on 0.5 liters per hour
 
-function drink(timeTaken){
-  return Math.floor(timeTaken*0.5);
+function drink(time){
+  return Math.floor(time*0.5);
 }
 
 
-console.log(drink(2))
+console.log(drink(4))
